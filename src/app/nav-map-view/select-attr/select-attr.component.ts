78979756d9f5fe129b44a03a7cf32c53959df092
ZiweiYuan <ziweiyua@usc.edu>
2018-11-29 21:07:41 -0800

    fix no value bugs

--- src/app/nav-map-view/select-attr/select-attr.component.ts
+++ src/app/nav-map-view/select-attr/select-attr.component.ts
@@ -74,11 +74,14 @@ export class SelectAttrComponent implements OnInit{
             }
         }).then(function (data: any) {
             if (data.length == 0) {
-                return;
+                data = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+                    6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0,
+                    12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0,
+                    18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
             }
-            data = data[0];
-            //data = [data[0]['0'], data[0]['1'], data[0]['2'], data[0]['3'], data[0]['4'], data[0]['5']]
-            var max = d3.max(Object.values(data));
+            else {
+                data = data[0];
+            }
 
             // set axis
             var hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
